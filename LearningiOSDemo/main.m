ba35d529e23dffdce396a7493eb4b977691a7045
YangBinqi <yangbinqi@YangBinqis-Mac-mini.local>
2016-05-18 20:36:25 +0800

    init project

---
+++ LearningiOSDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearningiOSDemo
+//
+//  Created by YangBinqi on 18/5/16.
+//  Copyright © 2016年 YangBinqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
